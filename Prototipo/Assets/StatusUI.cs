fe127db8e3b0e6441b62f7856b54a221825306c4
isiwii <isi.pescer@gmail.com>
2023-11-11 12:25:50 -0300

    prueba2

---
+++ Prototipo/Assets/StatusUI.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusUI : MonoBehaviour
+{
+    public Image healBackground;
+    public healthText;
+
+    private CharacterStatus status;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StatusUI = FindObjectOfType<CharacterStatus>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       Actualize(status.health, status.maxHealth);
+    }
+
+    public void Actualize(int value, int maxValue)
+    {
+        healthText.text = value.ToString;
+
+        healBackground.fillAmount = (float)value / maxValue;
+    }
+}
