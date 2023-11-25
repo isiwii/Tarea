f566aabcab04d1560ff387bf658a912e335cf593
isiwii <isi.pescer@gmail.com>
2023-11-24 22:48:51 -0300

    tarea 24 Noviembre lista
    falta iconos items
    arreglar animaciones

--- Prototipo/Assets/Assets/Collectables/Star.cs
+++ Prototipo/Assets/Assets/Collectables/Star.cs
@@ -1,20 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Star : MonoBehaviour
 {
-    public GameObject pivot;
-
-    private void OnTriggerEnter(Collider other)
+    public static int pumpkin;
+    public GameObject text;
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        var characterStatus = other.GetComponent<CharacterStatus>();
+      
+    }
 
-        if (characterStatus != null)
+    void OnTriggerEnter(Collider Col)
+    {
+        if(Col.gameObject.tag == "pumpkin")
         {
-            characterStatus.stars++;
-            Destroy(pivot);
+           text.SetActive(true);
+          //  Col.gameObject.SetActive(false);
+            Destroy(Col.gameObject);
         }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
 
     }
 }
