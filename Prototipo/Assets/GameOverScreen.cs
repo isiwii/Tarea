fb41549285abf981744158ef70d5d171c1af5085
isiwii <isi.pescer@gmail.com>
2023-11-25 19:00:06 -0300

    Ahora si, tarea lista

--- Prototipo/Assets/GameOverScreen.cs
+++ Prototipo/Assets/GameOverScreen.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class GameOverScreen : MonoBehaviour
 {
-    public Text pointsText;
+    public TMP_Text pointsText;
     // Start is called before the first frame update
     public void Setup(int score)
     {
